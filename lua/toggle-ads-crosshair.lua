f9bdc748ed9ac7742fbdb1058e0fb7033a91813c
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-01-03 22:30:51 +0000

    Improve toggle ADS crosshair keybind

--- lua/toggle-ads-crosshair.lua
+++ lua/toggle-ads-crosshair.lua
@@ -1,5 +1,6 @@
 local setting = managers.user:get_setting("accessibility_dot_hide_ads")
-local message = "ADS crosshair " .. (setting and "enabled" or "disabled") .. "."
+local new_setting = not setting
+local message = "ADS crosshair " .. (new_setting and "disabled" or "enabled") .. "."
 
-managers.user:set_setting("accessibility_dot_hide_ads", not setting)
+managers.user:set_setting("accessibility_dot_hide_ads", new_setting)
 managers.chat:_receive_message(1, "OPTIONS", message, tweak_data.system_chat_color)
